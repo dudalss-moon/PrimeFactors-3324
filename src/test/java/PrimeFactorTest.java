609d5d6cfa83d4ef5b5b43f3f1198f6bfd4dfd05
dudalss <myemail@emap.com>
2025-06-04 10:22:15 +0900

    [feature] 1p

--- src/test/java/PrimeFactorTest.java
+++ src/test/java/PrimeFactorTest.java
@@ -1,9 +1,13 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrimeFactorTest {
     @Test
-    void name() {
-        assertEquals(1, 1);
+    void testPrimeFactor0f1() {
+        PrimeFactor primeFactor = new PrimeFactor();
+        assertEquals(Arrays.asList(), primeFactor.of(1));
     }
+}
